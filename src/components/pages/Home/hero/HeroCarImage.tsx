626ad33e36eb003eef26ab58517710212324461f
Jarvis (Dcodax) <jarvis@dcodax.com>
2026-05-21 06:04:52 +0000

    fix: prevent hero car cropping

--- src/components/pages/Home/hero/HeroCarImage.tsx
+++ src/components/pages/Home/hero/HeroCarImage.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export function HeroCarImage() {
   return (
-    <div className="relative flex w-full justify-center pt-0 lg:justify-end lg:self-start">
+    <div className="relative flex w-full justify-center pt-2 lg:justify-end lg:self-start lg:pt-4 xl:pt-6">
       <Image
         src="/assets/hero/car_with_shadow.png"
         alt="Car"
         width={980}
-        height={700}
+        height={760}
         priority
-        className="object-contain w-full max-w-[500px] sm:max-w-[600px] md:max-w-[680px] lg:max-w-[780px] xl:max-w-[900px] 2xl:max-w-[980px] lg:-translate-y-12 lg:translate-x-4 xl:-translate-y-16 xl:translate-x-6"
+        className="w-full max-w-[500px] object-contain sm:max-w-[600px] md:max-w-[680px] lg:max-w-[780px] lg:translate-x-4 xl:max-w-[900px] xl:translate-x-6 2xl:max-w-[980px]"
       />
     </div>
   );
